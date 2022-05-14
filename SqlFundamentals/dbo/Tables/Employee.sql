ef4f6624821d40599b4956ac9a124c52737e7475
v-asoliev <91322865+v-asoliev@users.noreply.github.com>
2022-05-14 22:15:21 +0500

    add tables and deployment script

---
+++ SqlFundamentals/dbo/Tables/Employee.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Id] INT IDENTITY NOT NULL PRIMARY KEY,
+	[AddressId] INT NOT NULL REFERENCES [dbo].[Address] (Id),
+	[PersonId] INT NOT NULL REFERENCES [dbo].[Person] (Id),
+	[CompanyName] NVARCHAR(20) NOT NULL,
+	[Position] NVARCHAR(30) NULL,
+	[EmployeeName] NVARCHAR(100) NULL
+)
